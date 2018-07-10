85559055a1cfbfbd5d78215e2d06c1dea65a59d0
igfa <fernandez.i@husky.neu.edu>
2018-07-10 02:06:58 -0400

    Implements FindUserByName and associated checks on registering

--- src/main/java/webdev/repositories/UserRepository.java
+++ src/main/java/webdev/repositories/UserRepository.java
@@ -2,6 +2,14 @@ package webdev.repositories;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.query.Param;
 import webdev.models.User;
-public interface UserRepository extends CrudRepository<User, Integer> {}
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.username=:username")
+    User findUserByUsername(@Param("username") String Username);
+}
 
--- src/main/java/webdev/services/UserService.java
+++ src/main/java/webdev/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import webdev.models.User;
 import webdev.repositories.UserRepository;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,11 +15,30 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-
+    //rewire register to this
     @PostMapping("/api/user")
     public User createUser(@RequestBody User user) {
         System.out.println(user.getUsername() +":"+ user.getPassword());
         return userRepository.save(user);
+    }
+
+    @PostMapping("/api/register")
+    public User register(@RequestBody User user, HttpSession session) {
+        if (this.findUserByUsername(user.getUsername()) == null) {
+            return userRepository.save(user);
+        } else {
+            return null;
+        }
+    }
+
+
+    @GetMapping("/api/user/{username}")
+    public User findUserByUsername(@PathVariable("username") String username) {
+        User data = userRepository.findUserByUsername(username);
+            if(data == null) {
+            return data;
+        }
+        return data;
     }
 
     @GetMapping("/api/user")
