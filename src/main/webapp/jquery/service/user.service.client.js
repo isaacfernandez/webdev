228f650ebe287a6195e8e17e76022ef31ba73aaa
igfa <fernandez.i@husky.neu.edu>
2018-07-11 02:38:35 -0400

    Update functionality complete

--- src/main/webapp/jquery/service/user.service.client.js
+++ src/main/webapp/jquery/service/user.service.client.js
@@ -1,39 +1,61 @@
 function UserService() {
     this.register = register;
     this.logon = logon;
+    this.logout = logout;
+    this.update = update;
     this.url =
         'http://localhost:8080/api/user';
     this.login =
         'http://localhost:8080/api/login';
-
+    this.profile =
+        'http://localhost:8080/api/profile'
 
     self = this;
 
 
     //Refactor, only difference is URL
     function register(user, pass) {
-        console.log("Sending " + JSON.stringify({username:user, password: pass}))
+        console.log("Sending " + JSON.stringify({username: user, password: pass}))
         fetch(self.url, {
             method: 'post',
-            body: JSON.stringify({ username: user, password: pass}),
+            body: JSON.stringify({username: user, password: pass}),
             headers: {
                 'content-type': 'application/json'
             }
-        }).then( function(resp) {
-            console.log("Sent the following:" + JSON.stringify({ username: user, password: pass}));
-            return resp.json()});
+        }).then(function (resp) {
+            console.log("Sent the following:" + JSON.stringify({username: user, password: pass}));
+            return resp.json()
+        });
     }
 
     function logon(user, pass) {
-        console.log("Sending " + JSON.stringify({username:user, password: pass}))
-        fetch(self.login, {
+        console.log("Sending " + JSON.stringify({username: user, password: pass}))
+        return fetch(self.login, {
             method: 'post',
-            body: JSON.stringify({ username: user, password: pass}),
+            body: JSON.stringify({username: user, password: pass}),
             headers: {
                 'content-type': 'application/json'
             }
-        }).then( function(resp) {
-            console.log("Sent the following:" + JSON.stringify({ username: user, password: pass}));
-            return resp.json()});
+        }).then(function (resp) {
+            return resp.json()
+        });
     }
+
+    function logout() {
+
+    }
+
+    function update(user) {
+        return fetch(this.profile, {
+            method: 'put',
+            body: JSON.stringify(user),
+            'credentials': 'include',
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(function (resp) {
+            return resp.json()
+        });
+    }
+
 }
