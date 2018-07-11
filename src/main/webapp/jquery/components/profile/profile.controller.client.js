1635c97d7f47c293317441b1bff960567cf499e9
igfa <fernandez.i@husky.neu.edu>
2018-07-11 02:37:19 -0400

    Client side profile, update functionality

--- src/main/webapp/jquery/components/profile/profile.controller.client.js
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -3,16 +3,15 @@
     $(main);
 
     function main() {
-        $("#updateBtn").click(logout);
-        $("#logoutBtn").click(update);
+        $("#updateBtn").click(update);
+        $("#logoutBtn").click(logout);
         fetchProfile().then(function (user) {
-            console.log('promise run?');
             $("#usernameFld").val(user["username"]);
             $("#emailFld").val(user["email"]);
-            $("#firstnameFld").val(user["firstname"]);
-            $("#lastnameFld").val(user["lastname"]);
+            $("#firstnameFld").val(user["firstName"]);
+            $("#lastnameFld").val(user["lastName"]);
             $("#phoneFld").val(user["phone"]);
-            $("#dobFld").val(user["dob"]);
+            $("#dobFld").val(user["dateOfBirth"]);
             $("#roleFld").val(user["role"]);
             console.log("Values set");
         });
@@ -26,7 +25,23 @@
 
 
     function update() {
+        var user = new User();
+        user["username"] = $("#usernameFld").val();
+        user["email"] = $("#emailFld").val();
+        user["firstName"] = $("#firstnameFld").val();
+        user["lastName"] = $("#lastnameFld").val();
+        user["phone"] = $("#phoneFld").val();
+        user["dateOfBirth"] = $("#dobFld").val();
+        user["role"] = $("#roleFld").val();
+        userService.update(user).then(updateSuccess, updateFail);
+    }
 
+    function updateSuccess(){
+        console.log('update good!');
+    }
+
+    function updateFail() {
+        console.log('update failed');
     }
 
     function fetchProfile() {
