66adf386a66db14671d3c5a34c7105e30e807eaf
root <root@localdomain>
2018-08-17 02:21:36 -0400

    Widget boilerplate

---
+++ src/main/java/webdev/models/Widget.java
@@ -0,0 +1,112 @@
+package webdev.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity(name = "widget")
+public class Widget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String type;
+    private String data;
+    private String size;
+    private String link;
+    private String source;
+    private String listItems;
+    private String listType;
+    private int order;
+    @ManyToOne
+    @JsonIgnore
+    private Lesson lesson;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getListItems() {
+        return listItems;
+    }
+
+    public void setListItems(String listItems) {
+        this.listItems = listItems;
+    }
+
+    public String getListType() {
+        return listType;
+    }
+
+    public void setListType(String listType) {
+        this.listType = listType;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public Lesson getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Lesson lesson) {
+        this.lesson = lesson;
+    }
+}
---
+++ src/main/java/webdev/repositories/WidgetRepository.java
@@ -0,0 +1,13 @@
+package webdev.repositories;
+
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import webdev.models.Lesson;
+import webdev.models.Widget;
+
+
+public interface WidgetRepository extends CrudRepository<Widget, Integer> {
+
+}
